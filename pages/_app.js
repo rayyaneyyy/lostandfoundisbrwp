fab6a86471d051ea8fabf13fbd5161fddb411c6c
rayyaneyyy <rayyanasimx@icloud.com>
2025-06-22 05:24:07 +0500

    Fix navbar overlapping content

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,13 @@ import { ThemeProvider } from "next-themes";
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <Navbar />
-      <Component {...pageProps} />
+      <div className="min-h-screen bg-white dark:bg-zinc-900 text-zinc-800 dark:text-white">
+        <Navbar />
+        {/* Add padding so content is not hidden behind navbar */}
+        <main className="pt-20 px-4 sm:px-8">
+          <Component {...pageProps} />
+        </main>
+      </div>
     </ThemeProvider>
   );
 }
